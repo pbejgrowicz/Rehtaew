75f0adc3da50eadbde6fc9adc196567c32814833
Speednet <user1@mac-mini-user.local>
2016-07-12 08:32:49 +0200

    Initial Commit

---
+++ Rehteaw/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Rehteaw
+//
+//  Created by Speednet on 12.07.2016.
+//  Copyright © 2016 Speednet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Rehteaw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rehteaw
+//
+//  Created by Speednet on 12.07.2016.
+//  Copyright © 2016 Speednet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
